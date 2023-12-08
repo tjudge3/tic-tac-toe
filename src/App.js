df422ccb35e1b09c3bc732b0f3013d5d481a12d3
tjudge3 <67015636+tjudge3@users.noreply.github.com>
2023-12-08 00:36:55 -0500

    Added setValue

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState} from 'react';
 function Square(){
  const [value, setValue] = useState(null);
     function handleClick(){
-        console.log('clicked');
+        setValue('X')
     }
     return <button className="square" onClick={handleClick}>{value}</button>;
 }
