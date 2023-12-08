9338566e47a7454f6181e49e4f25a853e6f485e6
tjudge3 <67015636+tjudge3@users.noreply.github.com>
2023-12-08 00:19:45 -0500

    Added onClick handler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 function Square({value}){
-    return <button className="square">{value}</button>;
+    function handleClick(){
+        console.log('clicked');
+    }
+    return <button className="square" onClick={handleClick}>{value}</button>;
 }
 
 export default function Board() {
