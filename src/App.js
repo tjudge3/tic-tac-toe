85fe95ed6ecc8ea0b068aa144f24a128372abfb7
tjudge3 <67015636+tjudge3@users.noreply.github.com>
2023-12-09 17:13:34 -0500

    Implemented move history system

--- src/App.js
+++ src/App.js
@@ -54,19 +54,36 @@ function Board({ xIsNext, squares, onPlay}) {
 export default function Game(){
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares){
     setHistory([...history, nextSquares]);
     setXIsNext(!xIsNext);
   }
+  function jumpTo(nextMove){
+      // TODO
+  }
+  const moves = history.map((squares, move) => {
+      let description;
+      if (move > 0) {
+          description = 'Go to move #' + move;
+      } else {
+          description = 'Go to game start';
+      }
+      return (
+          <li key={move}>
+            <button onClick={() => jumpTo(move)}>{description}</button>
+          </li>
+      );
+      });
     return(
         <div className="game">
             <div className="game-board">
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
             </div>
             <div className="game-info">
-                <ol>{/*TODO*/}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
     );
