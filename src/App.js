682333c9723037d04fa283a09d9e77474e69e809
tjudge3 <67015636+tjudge3@users.noreply.github.com>
2023-12-09 16:13:10 -0500

    If square is already filled, return early

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ export default function Board() {
  const [xIsNext, setXIsNext] = useState(true);
  const [squares, setSquares] = useState(Array(9).fill(null));
  function handleClick(i){
+     if (squares[i]) {
+         return;
+     }
      const nextSquares = squares.slice();
      if (xIsNext){
          nextSquares[i] = "X";
