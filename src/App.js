3ab5ed217cfca56f8481f2ed66591b18e74728f6
tjudge3 <67015636+tjudge3@users.noreply.github.com>
2023-12-09 16:08:03 -0500

    Added Turn switch with X's and O's

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ function Square({value, onSquareClick}){
 }
 
 export default function Board() {
+ const [xIsNext, setXIsNext] = useState(true);
  const [squares, setSquares] = useState(Array(9).fill(null));
  function handleClick(i){
      const nextSquares = squares.slice();
+     if (xIsNext){
+         nextSquares[i] = "X";
+     } else {
+         nextSquares[i] = "O";
+     }
      nextSquares[i] = "X";
      setSquares(nextSquares);
  }
